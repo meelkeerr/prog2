accd1f22bfbff58da0c5a0fbfc4d4bdaa8357695
meelkeerr <melker.falting.8772@student.uu.se>
2023-10-11 20:11:03 +0200

    testar igen

--- MA4_2.py
+++ MA4_2.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-# from person import Person
+from person import Person
 
-# def main():
-# 	f = Person(5)
-# 	print(f.get())
-# 	f.set(7)
-# 	print(f.get())
+def main():
+	f = Person(5)
+	print(f.get())
+	f.set(7)
+	print(f.get())
 
-# if __name__ == '__main__':
-# 	main()
-
-print('hello word')
+if __name__ == '__main__':
+	main()
