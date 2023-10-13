7aa8de33fee226f240735c0dd635b940ed168c57
meelkeerr <melker.falting.8772@student.uu.se>
2023-10-13 13:45:12 +0200

    fds

--- MA4_2.py
+++ MA4_2.py
@@ -12,25 +12,25 @@ def fib_py(n):
 		return fib_py(n - 1) + fib_py(n - 2)
 
 def main():
-	fib_seq = [fib_numba(n) for n in range(20, 31)]
+	fib_seq = [fib_numba(n) for n in range(20, 41)]
 	t_py = []
 	t_numba = []
 	t_cpp = []
 	
 	start = pc()
-	for n in range(20, 31):
+	for n in range(20, 41):
 		fib_py(n)
 		end = pc()
 		t_py.append(end - start)
 
 	start = pc()
-	for n in range(20, 31):
+	for n in range(20, 41):
 		fib_numba(n)
 		end = pc()
 		t_numba.append(end - start)
 	p = Person(1)
 	start = pc()
-	for n in range(20, 31):
+	for n in range(20, 41):
 		p.set(n)
 		p.fib()
 		end = pc()
@@ -43,8 +43,8 @@ def main():
 	plt.grid()
 	plt.legend()
 	plt.savefig('ma4_2.png')
-	print(fib_numba(47))
-	p.set(47)
+	print(fib_numba(10))
+	p.set(10)
 	print(p.fib())
 
 	
