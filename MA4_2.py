d41e74bbedcaa362f87309fbea1ce29f9eb465c7
meelkeerr <melker.falting.8772@student.uu.se>
2023-10-13 11:45:10 +0200

    made plot

--- MA4_2.py
+++ MA4_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+from matplotlib import pyplot as plt
 from numba import njit
 from person import Person
+from time import perf_counter as pc
+from time import sleep as pause
 
 def fib_py(n):
 	if n <= 1:
@@ -8,21 +11,48 @@ def fib_py(n):
 	else:
 		return fib_py(n - 1) + fib_py(n - 2)
 
+def main():
+	fib_seq = [fib_numba(n) for n in range(30, 46)]
+	t_py = []
+	t_numba = []
+	t_cpp = []
+	
+	start = pc()
+	for n in range(30, 46):
+		fib_py(n)
+		end = pc()
+		t_py.append(end - start)
+
+	start = pc()
+	for n in range(1, 46):
+		fib_numba(n)
+		end = pc()
+		t_numba.append(end - start)
+	p = Person(0)
+	start = pc()
+	for n in range(30, 46):
+		p.set(n)
+		p.fib()
+		end = pc()
+		t_cpp.append(end - start)
+		
+	
+	plt.plot(t_py, fib_seq, label='python')
+	plt.plot(t_numba, fib_seq, label='numba')
+	plt.plot(t_cpp, fib_seq, label='C++')
+	plt.grid()
+	plt.legend()
+	plt.savefig()
+
+	
+			
+
 @njit
 def fib_numba(n):
 	if n <= 1:
 		return n
 	else:
 		return (fib_numba(n - 1) + fib_numba(n - 2))
-	
-def main():
-	f = Person(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-	print(fib_numba(6))
-	print(fib_py(6))
-	
 
 if __name__ == '__main__':
 	main()
